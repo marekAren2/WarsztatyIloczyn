0cd8c925145d4872b1590ff41148ff4ff4a99a58
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-09 19:59:10 +0000

    last

--- script.js
+++ script.js
@@ -11,12 +11,11 @@
 // }
 
 // skrocony zapis funkcji z jedna metoda?! return zwraca wynik metod jak jedna nie trzeba return ale i nawiasow z klamra ?
-// const iloczyn = (num1, num2 ,num3) => {num1 * num2 * num3}; //undefined
+// const iloczyn = (num1, num2, num3) => { num1 * num2 * num3 }; //undefined wynik
 
 const iloczyn = (num1, num2, num3) => num1 * num2 * num3;
 
-let a = 0; //test commit to github repository
-let b = 0; //test commit to github repository test2
+
 
 let wynikIloczynu = iloczyn(3, 10, 2);
 console.log(wynikIloczynu);
